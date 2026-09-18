9a46ebe45ccb852a3551ab3ad5d54cdf5fc71619
chahed mazouz <mazouzchahd12@gmail.com>
2026-09-18 15:02:12 +0100

    fix: repaire a bug in filters by city mismatchin and fix the problem of to upercase and lowercase

--- js/filters.js
+++ js/filters.js
@@ -1,32 +1,23 @@
-function filterOffer(offres, filters) {
+function filterOffers(offres, filters) {
+
     return offres.filter(offer => {
 
-        const matchesCity = !filters.city || offer.ville === filters.city
+        const matchesCity = !filters.city || offer.ville.toLowerCase() === filters.city.toLowerCase();
 
-        const matchesContract = !filters.contract || offer.typeContract === filters.contract
+        const matchesContract =
+            !filters.contract ||
+            offer.typeContrat.toLowerCase() === filters.contract.toLowerCase();
 
-        const matchesTechnology = !filters.technology || offer.includes(filters.technology)
+        const matchesTechnology =
+            !filters.technology ||
+            offer.technologies.some(technology => technology.toLowerCase() === filters.technology.toLowerCase()
+            );
 
-        const matchesSearch =!filters.titre.toLowercase().includes(filters.search.toLowercase()) || 
-                             !filters.entreprise.toLowercase().includes(filters.search.toLowercase()) || 
-                             !filters.descriptionCourte.toLowercase().includes(filters.search.toLowercase()) 
+        const matchesSearch = !filters.search ||
+            offer.titre.toLowerCase().includes(filters.search.toLowerCase()) ||
+            offer.entreprise.toLowerCase().includes(filters.search.toLowerCase()) ||
+            offer.descriptionCourte.toLowerCase().includes(filters.search.toLowerCase());
 
-        return matchesCity && matchesContract && matchesTechnology && matchesSearch
-    })
-}   
-
-function sortOffersByDate(offers) {
-    return [...offers].sort((a, b) => {
-        return new Date(b.datePublication) - new Date(a.datePublication)
-    })
-}
-
-
-function resetFilters() {
-    return {
-        search: "",
-        technology: "",
-        city: "",
-        contract: ""
-    }
+        return (matchesCity && matchesContract && matchesTechnology && matchesSearch)
+    });
 }
